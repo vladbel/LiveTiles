1ca357befa3029516063f6dfe91d216246e55ab9
Vladimir Beliaev <vladbel@live.com>
2015-07-21 17:17:11 -0500

    PeekImageTile and Badge implementation

--- LiveTiles.Background/TileUpdateBackgroundTask.cs
+++ LiveTiles.Background/TileUpdateBackgroundTask.cs
@@ -25,166 +25,163 @@ namespace LiveTiles.Background
             BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
 
             //get tile view model
-            var viewModel = await GetTileViewModel();
+            var data = await GetTileData();
 
             // Update the live tile with the feed items.
-            viewModel.UpdateTile();
+            ITileViewModel tile = new TileViewModel();
+            tile.UpdateTile(data);
+
+            var badge = new BadgeViewModel();
+            badge.UpdateBadge(data);
 
             // Inform the system that the task is finished.
             deferral.Complete();
         }
 
-        private static async Task<ITileViewModel> GetTileViewModel()
+        private static async Task<ITileDataModel> GetTileData()
         {
 
-            //return new TileSquareTextViewModel();
-
-            return new TileWideImageAndTextViewModel();
+            var tileDataModel =  new TileDataModel();
+            return await Task.FromResult<ITileDataModel>(tileDataModel);
 
         }
 
+    }
+    public interface ITileDataModel
+    {
+        string SmallImage { get; }
+        string SquareImage { get; }
+        string WideImage { get; }
+        string Badge { get; }
+        string[] Notifications { get; }
+    }
+
+    public sealed class TileDataModel: ITileDataModel
+    {
+         private int _invocationCount;
+
+        public TileDataModel()
+        {
+            this._invocationCount = DateTime.Now.Second;
+        }
+
+
+
+        public string Badge
+        {
+            get
+            {
+                return (++_invocationCount).ToString();
+            }
+        }
+
+        public string[] Notifications
+        {
+            get
+            {
+                string[] result = { String.Format("First: {0} ", (++_invocationCount).ToString()), 
+                                    String.Format("Mid: {0} ", (++_invocationCount).ToString()), 
+                                    String.Format("End: {0} ", (++_invocationCount).ToString()) };
+                return result;
+            }
+        }
+
+        public string SmallImage
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string SquareImage
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string WideImage
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 
     public interface ITileViewModel
     {
-        string BackgroundImage { get; }
-        string Badge { get; }
-        string[] Notifications { get; }
 
-        void UpdateTile();
+        void UpdateTile(ITileDataModel data);
     }
 
-    public sealed class TileSquareTextViewModel : ITileViewModel
+    public sealed class TileViewModel : ITileViewModel
     {
-        private int _invocationCount;
 
-        public TileSquareTextViewModel()
-        {
-            this._invocationCount = DateTime.Now.Second;
-        }
+        public void UpdateTile(ITileDataModel data)
+        {            
 
-        public string BackgroundImage
-        {
-            get
-            {
-                return "";
-            }
-        }
+            string tileXmlString01 = @"
+                    <tile>
+                        <visual version='3'>
+                            <binding template='TileSquare71x71Image'>
+                                <image id='1' src='ms-appx:///Assets/Square71x71Logo.png' alt='Gray image'/>
+                            </binding>
+                            <binding template='TileSquare150x150PeekImageAndText01' fallback='TileSquareImage'>
+                                <image id='1' src='ms-appx:///Assets/Square150x150Logo.png' alt='Gray image'/>
+                                <text id='1'>S: 111111</text>
+                                <text id='2'>S: 222222</text>
+                                <text id='3'>S: 333333</text>
+                                <text id='4'>S: 444444</text>
+                             </binding>
+                            <binding template='TileWide310x150PeekImageAndText02' fallback='TileWideImageAndText01'>
+                                <image id='1' src='ms-appx:///Assets/WideLogo.scale-100.png' alt='Red image'/>
+                                <text id='1'>W: 11111</text>
+                                <text id='2'>W: 22222</text>
+                                <text id='3'>W: 33333</text>
+                                <text id='4'>W: 44444</text>
+                            </binding>
+                        </visual>
+                   </tile>";
 
-        public string Badge
-        {
-            get
-            {
-                return (++_invocationCount).ToString();
-            }
-        }
-
-        public string[] Notifications
-        {
-            get
-            {
-                string[] result = { String.Format("First: {0} ", (++_invocationCount).ToString()), 
-                                    String.Format("Mid: {0} ", (++_invocationCount).ToString()), 
-                                    String.Format("End: {0} ", (++_invocationCount).ToString()) };
-                return result;
-            }
-        }
-
-        public void UpdateTile()
-        {
-            // Create a tile update manager for the specified syndication feed.
-            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
-            updater.EnableNotificationQueue(true);
-            updater.Clear();
-
-            // Keep track of the number feed items that get tile notifications. 
-            int itemCount = 0;
-
-            // Create a tile notification for each feed item.
-            var notifications = this.Notifications;
-            foreach (var item in notifications)
-            {
-
-                // XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Block); // works
-
-                XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text01); //  works
-
-                //XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare310x310ImageAndText01); // NOT
-
-                //XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150ImageAndText02);
-
-                tileXml.GetElementsByTagName("text")[0].InnerText = item;
-
-                // Create a new tile notification. 
-                updater.Update(new TileNotification(tileXml));
-
-                // Don't create more than 5 notifications.
-                if (itemCount++ > 5) break;
-            }
-        }
-    }
-
-    public sealed class TileWideImageAndTextViewModel : ITileViewModel
-    {
-        private int _invocationCount;
-
-        public TileWideImageAndTextViewModel()
-        {
-            this._invocationCount = DateTime.Now.Second;
-        }
-
-        public string BackgroundImage
-        {
-            get
-            {
-                return "Assets\\WideLogo.scale-100.png";
-            }
-        }
-
-        public string Badge
-        {
-            get
-            {
-                return (++_invocationCount).ToString();
-            }
-        }
-
-        public string[] Notifications
-        {
-            get
-            {
-                string[] result = { String.Format("First: {0} ", (++_invocationCount).ToString()), 
-                                    String.Format("Mid: {0} ", (++_invocationCount).ToString()), 
-                                    String.Format("End: {0} ", (++_invocationCount).ToString()) };
-                return result;
-            }
-        }
-
-        public void UpdateTile()
-        {
-            // Create a tile update manager for the specified syndication feed.
-            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
-            updater.EnableNotificationQueue(true);
-            updater.Clear();
-
-            // Keep track of the number feed items that get tile notifications. 
-            int itemCount = 0;
-
-            // Create a tile notification for each feed item.
-            var notifications = this.Notifications;
+            string tileXmlString02 = @"
+                    <tile>
+                        <visual version='3'>
+                            <binding template='TileSquare71x71Image'>
+                                <image id='1' src='ms-appx:///Assets/Square71x71Logo.png' alt='Gray image'/>
+                            </binding>
+                            <binding template='TileSquare150x150Image' fallback='TileSquareImage'>
+                                <image id='1' src='ms-appx:///Assets/Square150x150Logo.png' alt='Gray image'/>
+                             </binding>
+                            <binding template='TileWide310x150ImageAndText01' fallback='TileWideImageAndText01'>
+                                <image id='1' src='ms-appx:///Assets/WideLogo.scale-100.png' alt='Red image'/>
+                                <text id='1'>This tile notification uses ms-appx images</text>
+                            </binding>
+                        </visual>
+                   </tile>";
 
 
-            XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150ImageAndText01);
-            tileXml.GetElementsByTagName("text")[0].InnerText = this.Notifications[0];
-
-            var tileImage = tileXml.GetElementsByTagName("image")[0] as XmlElement;
-            tileImage.SetAttribute("src", "ms-appx:///Assets/WideLogo.scale-100.png");
 
             // Create a new tile notification. 
+            XmlDocument tileXml = new Windows.Data.Xml.Dom.XmlDocument();
+            tileXml.LoadXml(tileXmlString01);
+
+            // Create a tile update manager for the specified syndication feed.
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.EnableNotificationQueue(true);
+            updater.Clear();
             updater.Update(new TileNotification(tileXml));
-
-
 
         }
     }
+
+    public sealed class BadgeViewModel
+    {
+        public void UpdateBadge (ITileDataModel data)
+        {
+            string tileXmlString01 = "<badge version='1' value='" + data.Badge + "'/>";
+
+
+
+            // Create a new tile notification. 
+            XmlDocument badgeXml = new Windows.Data.Xml.Dom.XmlDocument();
+            badgeXml.LoadXml(tileXmlString01);
+            BadgeUpdateManager.CreateBadgeUpdaterForApplication().Update(new BadgeNotification(badgeXml));
+        }
+    }
+
+ 
 }
